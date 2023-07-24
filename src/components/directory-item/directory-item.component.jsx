b89af40a65c59990d456f85a36724350525ab337
lingeorge88 <lingeorge04@gmail.com>
2023-07-24 16:00:55 -0700

    added styled components for cart icon and directory item

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,20 +1,16 @@
-import './directory-item.styles.scss';
+import {DirectoryItemContainer, Body, BackgroundImage}from'./directory-item.styles.jsx';
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
   return (
-    <div className='directory-item-container'>
-      <div
-        className='background-image'
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
+    <DirectoryItemContainer>
+      <BackgroundImage imageUrl={imageUrl}
       />
-      <div className='body'>
+      <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </Body>
+    </DirectoryItemContainer>
   );
 };
 
