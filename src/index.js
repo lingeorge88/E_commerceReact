f55f9419bd310542e9b58765d0fcbbdedf55d20d
lingeorge88 <lingeorge04@gmail.com>
2023-08-07 13:30:17 -0700

    added redux persist library to help persist data in local storage

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import { CartProvider } from './contexts/cart.context';
-import { store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={ store }>
-    <BrowserRouter>
-      {/* <CartProvider> */}
-    <App />
-    {/* </CartProvider> */}
-    </BrowserRouter>
+    <Provider loading={null} store={ store }>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+        </PersistGate>
     </Provider>
   </React.StrictMode>
   
