be1a0b1554764bea9c1b44cc289c7b6dbc34bf09
lingeorge88 <lingeorge04@gmail.com>
2023-08-08 20:06:28 -0700

    migrated user signin to sagas

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -4,7 +4,7 @@ import USER_ACTION_TYPES from './user.types';
 
 import { signInSuccess, signInFailed } from './user.action';
 
-import { getCurentUser, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { getCurentUser, createUserDocumentFromAuth, signInWithGooglePopup, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
 
 export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
     try {
@@ -19,6 +19,24 @@ export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
     }
   }
 
+export function* signInWithGoogle() {
+    try {
+    const { user } = yield call(signInWithGooglePopup);
+    yield call(getSnapshotFromUserAuth, user);
+    } catch (error) {
+     yield put(signInFailed(error));   
+    }
+}
+
+export function* signInWithEmail({ payload: { email, password}}) {
+    try {
+      const { user } = yield call(signInAuthUserWithEmailAndPassword, email, password);
+      yield call(getSnapshotFromUserAuth, user);
+    } catch (error) {
+      yield put(signInFailed(error));  
+    }
+}
+
 export function* isUserAuthenticated() {
     try {
         const userAuth = yield call(getCurentUser);
@@ -29,10 +47,18 @@ export function* isUserAuthenticated() {
     }
 }
 
+export function* onGoogleSignInStart () {
+    yield takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle);
+}
+
+export function* onEmailSignInStart () {
+    yield takeLatest(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, signInWithEmail);
+}
+
 export function* onCheckUserSession() {
     yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
 }
 
 export function* userSagas() {
-    yield all([call(onCheckUserSession)])
+    yield all([call(onCheckUserSession), call(onGoogleSignInStart), call(onEmailSignInStart)]);
 }
